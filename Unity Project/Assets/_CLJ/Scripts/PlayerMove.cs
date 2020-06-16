3d0e0e828c0d59095901c42aa4f971f34070ea99
chldlwns12 <chldlwns12@gmail.com>
2020-06-16 16:14:08 +0900

    2020.06.16 -레이캐스트를 이용한 총알 발사 -AddForce를 이용한 수류탄 투척

--- Unity Project/Assets/_CLJ/Scripts/PlayerMove.cs
+++ Unity Project/Assets/_CLJ/Scripts/PlayerMove.cs
@@ -8,6 +8,12 @@ public class PlayerMove : MonoBehaviour
 
     CharacterController cc;
 
+    //중력적용
+    public float gravity = -20;
+    float velocityY;    //낙하속도(벨로시티는 방향과 힘을 들고 있다)
+    float jumpPower = 10.0f;
+    int jumpCount = 0;
+
     private void Start()
     {
         cc = GetComponent<CharacterController>();
@@ -32,6 +38,37 @@ public class PlayerMove : MonoBehaviour
         //캐릭터컨트롤러는 충돌감지만 하고 물리가 적용안된다
         //따라서 충돌감지를 하기 위해서는 반드시
         //캐릭터컨트롤러 컴포넌트가 제공해주는 함수로 이동처리해야 한다
+        //cc.Move(dir * PlayerSpeed * Time.deltaTime);
+
+        //중력적용하기
+        
+
+        //캐릭터 점프
+        //점프버튼을 누르면 수직속도에 점프파워를 넣는다
+        //땅에 닿으면 0으로 초기화
+        //CollisionFlags.Above;
+        //CollisionFlags.Below;
+        //CollisionFlags.Sides;
+        //if(cc.isGrounded)//땅에 닿았냐?
+        //{
+        //    velocityY = 0;
+        //    jumpCount = 0;
+        //}
+        if (cc.collisionFlags == CollisionFlags.Below)
+        {
+            velocityY = 0;
+            jumpCount = 0;
+        }
+        else
+        {
+            velocityY += gravity * Time.deltaTime;
+            dir.y = velocityY;
+        }
+        if(Input.GetButtonDown("Jump") && jumpCount < 2)
+        {
+            velocityY = jumpPower;
+            jumpCount++;
+        }
         cc.Move(dir * PlayerSpeed * Time.deltaTime);
     }
 }
